faafd91b7a610ea19a06a18241041aea6757f44c
cshinaver <bshinaver@gmail.com>
2016-04-28 01:42:15 -0400

    Added game class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from view import View
 from event_manager import EventManager, TickEvent
+from game import Game
 from controller import KeyboardController
 from utils import apply_fn
 
@@ -15,12 +16,11 @@ def main():
     ev = EventManager()
     keybd = KeyboardController()
     view = View(WIDTH, HEIGHT)
+    game = Game(ev)
     apply_fn(
         lambda x: ev.register_listener(x),
-        [keybd, view],
+        [keybd, view, game],
     )
-
-    while 1:
-        ev.post(TickEvent())
+    game.run()
 
 main()
